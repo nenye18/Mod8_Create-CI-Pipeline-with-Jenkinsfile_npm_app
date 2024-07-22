068e4a0ed51edf20b8c66d631691f98e746e647f
Chinenye Nwagbara <chinenye.nw@gmail.com>
2024-07-22 10:19:37 -0500

    Created script.groovy for shared lib

---
+++ script.groovy
@@ -0,0 +1,17 @@
+def VersionUpdate() {
+    echo 'updating the version'
+}
+
+def TestApp() {
+    echo 'building the image'
+}
+
+def buildImage() {
+    echo 'building the image'
+}
+
+def CommitToGit() {
+  echo 'committing to git'
+}
+
+return this
