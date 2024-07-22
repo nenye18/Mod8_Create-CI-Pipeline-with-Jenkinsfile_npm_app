dc76024b9dab4ee42b7d260249e562a76600587f
Chinenye Nwagbara <chinenye.nw@gmail.com>
2024-07-22 12:13:31 -0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -28,7 +28,7 @@ def BuildImage(){
 def CommitToGit() {
     echo 'commit new version to git'
     withCredentials([usernamePassword(credentialsId: 'GitHub credentials',passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        sh 'git config --global user.email "chinenye.nw@gmail.com"                     
+        sh 'git config --global user.email "chinenye.nw@gmail.com" '                    
         sh 'git config --global user.name "nenye18" '
         sh 'git remote set-url origin https://$USER:$PASS@github.com/nenye18/Jenkins-project.git'
         sh 'git add .'
