74a9f3a8a7a15816e79ace319e5c175b1164b128
Chinenye Nwagbara <chinenye.nw@gmail.com>
2024-07-22 10:43:01 -0500

    Test VersionUpdate

--- script.groovy
+++ script.groovy
@@ -1,6 +1,12 @@
 def VersionUpdate() {
     echo 'updating the version'
-}
+    dir("app") {
+        sh "npm version patch"  //update the package.json version
+        def Version = readJSON(file: 'package.json').version
+        env.IMAGE_NAME ="$Version-$BUILD_NUMBER"                                    
+    }
+  }
+
 
 def TestApp() {
     echo 'building the image'
