b892c506ccfe5b05f1a3ff292bc2510bd867cf10
Chinenye Nwagbara <chinenye.nw@gmail.com>
2024-07-22 10:52:01 -0500

    testing build docker image

--- script.groovy
+++ script.groovy
@@ -17,8 +17,14 @@ def TestApp() {
 }
 
 def buildImage() {
-    echo 'building the image'
+    echo 'building the docker image...
+    withCredentials([usernamePassword(credentialsId: 'docker hub repository',passwordVariable: 'PASS', usernameVariable: 'USER')]){
+    sh 'docker build -t cnwagba/jenkins-repo-dockerhub:$IMAGE_NAME .'  //Build Docker Image
+    sh 'echo $PASS | docker login -u $USER --password-stdin'       // Authentication
+    sh 'docker push cnwagba/jenkins-repo-dockerhub:$IMAGE_NAME'       //Push to Docker repository
+    }
 }
+
 
 def CommitToGit() {
   echo 'committing to git'
